4654e4fe18746d8699794da306a4b7a81e32f8fb
marco <you@example.com>
2022-11-11 21:42:25 -0500

    .env port added

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ server.get("*",(req,res)=>{
 })
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
